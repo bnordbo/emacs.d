431427ad18494282f2e1dec1e4dbb7762918811d
Bjørnulf Geitekåsa <bg@snapp.local>
2016-02-06 10:03:54 +0100

    Keyboard shortcuts for moving lines up/down

--- lisp/init-keys.el
+++ lisp/init-keys.el
@@ -1,4 +1,8 @@
 (global-set-key (kbd "C-<tab>") 'other-window)
 (global-set-key (kbd "C-x a r")	'align-regexp)
+(global-set-key (kbd "M-<down>") 'md/move-lines-down)
+(global-set-key (kbd "M-<up>") 'md/move-lines-up)
+
+(ido-yes-or-no-mode)
 
 (provide 'init-keys)
