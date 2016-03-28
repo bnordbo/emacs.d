052fbdb50cdce1f6426b94627555c3b90024fe26
Bjørnulf Geitekåsa <bg@snapp.local>
2016-03-28 10:20:23 +0200

    Improved haskell indentation, 4 spaces

--- custom.el
+++ custom.el
@@ -5,12 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(haskell-indentation-layout-offset 4)
  '(haskell-indentation-left-offset 4)
- '(haskell-indentation-starter-offset 2)
- '(haskell-process-path-cabal "/usr/local/bin/cabal")
- '(haskell-process-path-ghci "/usr/local/bin/ghci")
- '(haskell-process-path-stack "/usr/local/bin/stack")
- '(haskell-process-type (quote stack-ghci))
- '(haskell-tags-on-save t))
+ '(haskell-indentation-starter-offset 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
