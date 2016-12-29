c07f43c293fd6cdb9ac4ee8e0e6b9b3d312cebfe
Bjørn Nordbø <bn@strangedays.no>
2016-12-29 16:22:53 +0100

    Switched to the InconsolataGo font

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (idomenu terraform-mode yaml-mode toml-mode move-dup markdown-mode magit ido-yes-or-no idle-highlight-mode hcl-mode hc-zenburn-theme haskell-mode go-mode edts cargo))))
+    (idomenu toml-mode todotxt-mode terraform-mode rainbow-mode rainbow-delimiters racer move-dup markdown-mode magit magit-filenotify ido-yes-or-no idle-highlight-mode hyai hcl-mode hc-zenburn-theme ghc-imported-from ghc elm-mode danneskjold-theme edts cargo))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-visual.el
+++ lisp/init-visual.el
@@ -1,5 +1,9 @@
+;; Frame
 (tool-bar-mode -1)
 (setq visible-bell t)
 (toggle-frame-fullscreen)
+
+;; Fonts
+(set-face-attribute 'default nil :family "InconsolataGo" :height 145)
 
 (provide 'init-visual)
