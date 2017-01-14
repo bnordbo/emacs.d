044b6b6624f8d7e7dde137af641f525ad79034fe
Bjørn Nordbø <bn@strangedays.no>
2017-01-14 13:10:57 +0100

    Merge custom.el

--- custom.el
+++ custom.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(haskell-indentation-layout-offset 4)
  '(haskell-indentation-left-offset 4)
- '(haskell-indentation-starter-offset 4))
+ '(haskell-indentation-starter-offset 4)
+ '(package-selected-packages
+   (quote
+    (intero toml-mode todotxt-mode rainbow-mode rainbow-delimiters racer move-dup magit-filenotify ido-yes-or-no idle-highlight-mode hyai hcl-mode hc-zenburn-theme ghc-imported-from ghc elm-mode danneskjold-theme cargo))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
