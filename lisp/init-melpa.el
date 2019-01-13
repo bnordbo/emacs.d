86abf3ae0940c43dbbdb60439eab1354ef4aadb5
Bjørn Nordbø <bn@strangedays.no>
2019-01-13 15:26:52 +0100

    Use beta melpa

--- lisp/init-melpa.el
+++ lisp/init-melpa.el
@@ -1,7 +1,7 @@
 (require 'package)
 
 (add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+             '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
 (provide 'init-melpa)
