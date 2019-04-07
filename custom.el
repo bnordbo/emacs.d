8a38c2e9b8cd3fe1e9187925e4b7c8bed2701855
Bjørn Nordbø <bn@strangedays.no>
2019-04-07 09:44:19 +0200

    Custom changes

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yaml-mode toml-mode terraform-mode solarized-theme smex rust-playground reverse-theme racer pastelmac-theme ox-gfm move-dup lsp-rust idomenu ido-yes-or-no idle-highlight-mode hydandata-light-theme hyai hc-zenburn-theme haskell-mode green-screen-theme graphviz-dot-mode go-mode github-pullrequest flycheck eproject edts deft corral company cargo bazel-mode autothemer alect-themes))))
+    (use-package toml-mode todotxt-mode smex rust-playground rainbow-mode rainbow-delimiters racer paredit move-dup magit-filenotify lsp-rust intero ido-yes-or-no idle-highlight-mode hindent hcl-mode hc-zenburn-theme go-mode ghc-imported-from ghc flymake-rust flycheck-rust elm-mode diminish danneskjold-theme cider cargo))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
