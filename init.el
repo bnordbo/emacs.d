c54b94c3df2e196875c7e6ae3e7d721831a5a8da
Bjørn Nordbø <bn@wgtwo.com>
2020-09-24 23:07:46 +0200

    Migrated to org babel config

--- init.el
+++ init.el
@@ -1,44 +1,17 @@
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-;(package-initialize)
+(require 'package)
+(package-initialize)
 
-(let ((default-directory "/usr/local/share/emacs/site-lisp/"))
-  (normal-top-level-add-subdirs-to-load-path))
-
-;; Environment
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-;(require 'exec-path-from-shell)
-;(exec-path-from-shell-initialize)
-
-;; Extra libraries
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(add-to-list 'load-path (expand-file-name "~/src/elisp"))
-
-(load-library "keywiz")
-
-(require 'init-bn)
-
-(require 'init-melpa)
-(require 'init-env)
-
-(require 'init-editor)
-(require 'init-git)
-;(require 'init-ido)
-(require 'init-helm)
-(require 'init-ispell)
-(require 'init-keys)
-(require 'init-org)
-(require 'init-theme)
-(require 'init-visual)
-
-(require 'init-go)
-(require 'init-haskell)
-(require 'init-java)
-(require 'init-rust)
-
-;; Third party libraries with local sources
-(add-to-list 'load-path (expand-file-name "~/src/elisp/intero/elisp"))
-
-(load-library "intero")
+(org-babel-load-file "~/.emacs.d/config.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(key-quiz zoom yaml-mode ws-butler whole-line-or-region visual-regexp-steroids use-package toml-mode terraform-mode solarized-theme rust-playground reverse-theme racer protobuf-mode pastelmac-theme ox-gfm org-pomodoro org-bullets org-attach-screenshot org move-dup memoize magit-popup lsp-rust kotlin-mode idomenu hydandata-light-theme hyai helm-tramp helm-smex helm-rtags helm-rg helm-org helm-c-yasnippet hc-zenburn-theme haskell-mode green-screen-theme graphviz-dot-mode go-playground go-fill-struct go-eldoc go-autocomplete go-add-tags github-pullrequest flycheck exec-path-from-shell eshell-autojump eproject edts dumb-jump deft corral company cargo buffer-move browse-at-remote bazel-mode autothemer alect-themes ace-jump-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
