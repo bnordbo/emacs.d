024f26b412ce17a484bf18be908080eb2751cf7a
Bjørn Nordbø <bn@wgtwo.com>
2020-10-09 21:53:36 +0200

    Moved function definitions out of the org config

---
+++ elisp/bn.el
@@ -0,0 +1,11 @@
+;; Open a new line below the current one and indent.  I missed this
+;; function for ten years.  It took me two minutes to whip together this
+;; one.
+
+(defun bn/next-line-and-indent ()
+  (interactive)
+  (end-of-line)
+  (default-indent-new-line))
+
+(defun bn/roam-files ()
+  (directory-files "~/Repository/Roam" t ".*org$"))
