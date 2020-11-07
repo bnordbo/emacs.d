3e4f85edc4d05b94da870a86d3f2ceb7173bf99a
Bjørn Nordbø <bn@wgtwo.com>
2020-11-07 20:28:30 +0100

    Support functions for finding agenda files recursively

--- elisp/bn.el
+++ elisp/bn.el
@@ -13,3 +13,18 @@
 (defun bn/other-window-back ()
   (interactive)
   (other-window -1))
+
+(defun bn/agenda-files-recursively (dir)
+  (directory-files-recursively dir "\.org$" nil
+                               (bn/agenda-file-fn dir 1 '("Archive"))))
+
+(defun bn/agenda-file-fn (base-dir max-depth &optional ignore)
+  "Returns a predicate that is true if the file passed to it
+should be considered by org-agenda. BASE-DIR is the base
+directory passed to directory-files-recursively, and MAX-DEPTH is
+the max depth to search. If IGNORE is specified, it will return
+false if the file matches a name in this list."
+  (lambda (file)
+    (let ((parts (split-string (replace-regexp-in-string base-dir "" file) "/" t)))
+      (and (not (member (car (last parts)) ignore))
+           (<= (length parts) max-depth)))))
