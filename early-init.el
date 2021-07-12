a25be873e5066f8d39c1f2367d39dbf0bd469d13
Bjørn Nordbø <bn@wgtwo.com>
2021-07-12 16:01:27 +0200

    Move GC to early init

--- early-init.el
+++ early-init.el
@@ -3,3 +3,6 @@
 ; - https://github.com/d12frosted/homebrew-emacs-plus/issues/187
 ; - https://github.com/d12frosted/homebrew-emacs-plus/issues/323
 (setenv "LIBRARY_PATH" "/usr/local/opt/gcc/lib/gcc/11:/usr/local/opt/libgccjit/lib/gcc/11:/usr/local/opt/gcc/lib/gcc/11/gcc/x86_64-apple-darwin20/11.1.0")
+
+; Increase GC threshold during startup.
+(setq gc-cons-threshold (* 100 1000 1000))
